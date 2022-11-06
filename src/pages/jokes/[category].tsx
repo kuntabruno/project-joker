e6ada0d044c1f5d5217704dd60f093e0f7d11857
kuntabruno <kuntabruno@gmail.com>
2022-11-06 17:32:57 +0300

    Added required pages

---
+++ src/pages/jokes/[category].tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function RandomJokeInCategoryPage() {
+    return (
+        <div>
+        <Head>
+          <title>Random Joke in particular Category</title>
+          <meta name="description" content="Random Joke in particular category" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+         <h1>Random Joke in particual category Page</h1>
+      </div>
+    );
+}
---
+++ src/pages/jokes/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function RandomJokePage() {
+    return (
+
+    <div>
+      <Head>
+        <title>Random Joke in all Categories</title>
+        <meta name="description" content="Random Joke in all Categories" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+       <h1>Random Joke Page</h1>
+    </div>
+    );
+}
